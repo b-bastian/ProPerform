2c2a408ea0ca8507d221854dadacbae9b810a1bd
cankie7474 <cankayacbon@gmail.com>
2026-01-22 12:42:38 +0100

    add dev menu and index redirect for dev mode

--- properform-expo/app/index.tsx
+++ properform-expo/app/index.tsx
@@ -3,6 +3,8 @@ import { useContext, useEffect, useState } from "react";
 import { OnboardingContext } from "@/src/context/OnboardingContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const DEV_MODE_STATUS = true; // false setzen für normal, true dev mode
+
 export default function Index() {
   const [ready, setReady] = useState(false);
   const [finished, setFinished] = useState(false);
@@ -16,6 +18,10 @@ export default function Index() {
   }, []);
 
   if (!ready) return null;
+
+  if (__DEV__ && DEV_MODE_STATUS) {
+    return <Redirect href="/dev-menu" />;
+  }
 
   if (!finished) {
     return <Redirect href="/(onboarding)/OnboardingScreen" />;

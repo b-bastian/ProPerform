6cc7946ab003dd81ca05e1cf8c69ad1cfb3c1649
b-bastian <bbrugger@schueler.hakzell.at>
2026-03-03 11:33:01 +0100

    added verify token route

---
+++ routes/AuthRoutes/protectedAuthRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import { requireAuth } from "../../../middleware/auth.js";
+
+const router = express.Router();
+
+router.post("/verify-token", requireAuth, async (req, res) => {
+  try {
+    res.status(200).json({ valid: true, uid: req.user.uid });
+  } catch (error) {
+    console.error("invalid token.:", error);
+    res.status(500).json({ message: "invalid token.", error: error.message });
+  }
+});
+
+export default router;

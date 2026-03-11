7768bb63cb5c2278b13318a3c3b98ddb0bf7376d
bastian-js <bruggi44@gmail.com>
2026-03-11 20:43:31 +0100

    mounted auth/refresh to router

--- routes/AuthRoutes/index.js
+++ routes/AuthRoutes/index.js
@@ -4,6 +4,7 @@ import userAuthRoutes from "./public/userAuthRoutes.js";
 import verificationRoutes from "./public/verificationRoutes.js";
 import passwordRoutes from "./public/passwordRoutes.js";
 import trainerAuthRoutes from "./public/trainerAuthRoutes.js";
+import tokenAuthRoutes from "./public/tokenAuthRoutes.js";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use(userAuthRoutes);
 router.use(verificationRoutes);
 router.use(passwordRoutes);
 router.use(trainerAuthRoutes);
+router.use(tokenAuthRoutes);
 
 export default router;

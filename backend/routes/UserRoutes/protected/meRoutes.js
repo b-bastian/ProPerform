2bb77cc1d007995968011f40b2d409568923f9d2
b-bastian <bbrugger@schueler.hakzell.at>
2026-03-26 08:49:41 +0100

    updated response

--- backend/routes/UserRoutes/protected/meRoutes.js
+++ backend/routes/UserRoutes/protected/meRoutes.js
@@ -77,7 +77,9 @@ router.get("/me/trainer", requireAuth, async (req, res) => {
         .json({ message: "no trainer found for this user." });
     }
 
-    return res.status(200).json({ trainers: rows[0] });
+    const trainer = rows[0];
+
+    return res.status(200).json({ trainer });
   } catch (err) {
     console.error("fetch trainer error.", err);
     res.status(500).json({ message: "failed to fetch trainer." });
